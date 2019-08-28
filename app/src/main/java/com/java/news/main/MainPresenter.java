a7165bca67231e5e942b209fb89b8075a3a70870
Lynne <fflinjj@163.com>
2019-08-28 11:34:35 +0800

    add some codes

--- app/src/main/java/com/java/news/main/MainPresenter.java
+++ app/src/main/java/com/java/news/main/MainPresenter.java
@@ -3,5 +3,15 @@ package com.java.news.main;
  * Created by ljf on 2019-8-28.
  */
 
-public interface MainPresenter {
+public class MainPresenter implements MainContract.Presenter {
+
+    @Override
+    public void switchNavigation() {
+
+    }
+
+    @Override
+    public void start() {
+
+    }
 }
