7b78eb72668c2ac7633c786a887957110dec869c
Lynne <fflinjj@163.com>
2019-09-05 15:57:45 +0800

    fix bug

--- app/src/main/java/com/java/news/news/newsDetail/NewsDetailPresenter.java
+++ app/src/main/java/com/java/news/news/newsDetail/NewsDetailPresenter.java
@@ -8,34 +8,58 @@ import com.java.news.data.NewsFavorEntity;
 import com.java.news.data.NewsHisEntity;
 import com.java.news.data.RealmHelper;
 
+import io.realm.Realm;
+
 public class NewsDetailPresenter implements NewsDetailContract.Presenter {
     private NewsDetailContract.View mView;
+    private Realm mRealm = Realm.getDefaultInstance();
     private RealmHelper dbHelper = RealmHelper.getInstance();
     public NewsDetailPresenter(NewsDetailContract.View view){
         mView = view;
     }
     @Override
     public void addHis(NewsEntity news) {
-        news.setRead(true);
-        dbHelper.insertOrUpdateNews(news);
+        mRealm.executeTransaction(new Realm.Transaction() {
+            @Override
+            public void execute(Realm realm) {
+                news.setRead(true);
+            }
+        });
+//        dbHelper.insertOrUpdateNews(news);
     }
 
     @Override
     public void addFavor(NewsEntity news) {
-        news.setFavor(true);
-        dbHelper.insertOrUpdateNews(news);
+        mRealm.executeTransaction(new Realm.Transaction() {
+            @Override
+            public void execute(Realm realm) {
+                news.setFavor(true);
+            }
+        });
+//        dbHelper.insertOrUpdateNews(news);
     }
 
     @Override
     public void removeHis(NewsEntity news) {
-        news.setRead(false);
-        dbHelper.insertOrUpdateNews(news);
+        mRealm.executeTransaction(new Realm.Transaction() {
+            @Override
+            public void execute(Realm realm) {
+                news.setRead(false);
+            }
+        });
+//        dbHelper.insertOrUpdateNews(news);
     }
 
     @Override
     public void removeFavor(NewsEntity news) {
-        news.setFavor(false);
-        dbHelper.insertOrUpdateNews(news);
+
+        mRealm.executeTransaction(new Realm.Transaction() {
+            @Override
+            public void execute(Realm realm) {
+                news.setFavor(false);
+            }
+        });
+//        dbHelper.insertOrUpdateNews(news);
     }
 
 
